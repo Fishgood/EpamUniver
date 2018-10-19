7b7d7060329f449828fb4847dce3ce4c4361fd74
Fishgood <nickandruschenko19@gmail.com>
2018-10-19 18:15:18 +0300

    deleted some comments

--- src/task2/ex1/Main.java
+++ src/task2/ex1/Main.java
@@ -4,25 +4,7 @@ import task2.ex1.controller.Controller;
 
 public class Main {
     public static void main(String[] args){
-        /*Controller controller = new Controller();
-
-        controller.run();*/
 
         new Controller().run();
-
-        /*int[] arr = {1, 3, 5, 7, 9, 11, 13, 15};
-        int res = 0;
-        String resString = "";
-        int a = 0;
-        int b = 8;
-
-        do {
-            resString = "";
-            res = arr[a + (int)(Math.random() * b)] + arr[a + (int)(Math.random() * b)] + arr[a + (int)(Math.random() * b)];
-            resString += res;
-            System.out.println(resString);
-        }while (res != 30);
-
-        System.out.println("end one: " + resString);*/
     }
 }
